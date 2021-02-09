af6d0acdeff46cfba614b69d969da2d1896cab03
paras <paraswadhwa.94@gmail.com>
2021-02-10 00:00:01 +0530

    scroll component removed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-// import Scroll from '../components/ScrollComponent';
 import Home from '../components/Home';
 
 const styles = {
